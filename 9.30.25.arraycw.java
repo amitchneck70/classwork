8473bc732fce4becda47b815e0d83905d31081c2
Amy Mitchneck <amitchneck70@stuy.edu>
2025-09-30 10:12:21 -0400

    fixed for loop to return a copy for returnCopy

--- 9.30.25.arraycw.java
+++ 9.30.25.arraycw.java
@@ -1,6 +1,10 @@
 // returnCopy returns new array with the same values as the parameter array
+class 9.30.25.arraycw{
 public static int[] returnCopy(int[] array){
-  for(i = 0; i<array.length;i++){
-    int [] copy = copy [0 + array[0+i]];}
-    return copy;
+  char [] copying = array[array.length];
+  myCopy = System.array.copy(copying, 0, stop, array.length);
+  for(i = 0; i < array.length;i++){
+    System.out.print(i + ",");
+  }
+    return myCopy;
   }
