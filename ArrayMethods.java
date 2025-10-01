d71ac46cb76e68a7783d40dda0051c021dc93b0f
Amy Mitchneck <amitchneck70@stuy.edu>
2025-10-01 10:14:36 -0400

    copied arr to string, added main method and test cases

---
+++ ArrayMethods.java
@@ -0,0 +1,51 @@
+// Amy Mitchneck amym4019@nycstudents.net
+// Alex Mok
+public class ArrayMethods{
+//2. Copy your arrToString method from before.
+/**Return a String that represets the array in the format:
+* "[2, 3, 4, 9]"
+* Note the comma+space between values, and between values
+*/
+public static String arrToString(int[]ary){
+  String ans "["";
+    for (int i = 0; i < nums.length; i++) {
+      ans += ary[i];
+      if ( i <n nums.length - 1 ) {
+        ans += ", ";
+      }
+    }
+    ans += "]";
+  return ans;
+}
+
+//3. Write arrToString, with a 2D array parameter.
+//Note: Different parameters are allowed with the same function name.
+/**Return a String that represets the 2D array in the format:
+  * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
+  * Note the comma+space between values, and between arrays
+  * You are encouraged to notice that you may want to re-use
+  * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
+  */
+public static String arrToString2D(int[][]ary){
+  //this method should call arrToString(int[])
+
+  return "";
+}
+
+/*Return the sum of all of the values in the 2D array */
+public static int arr2DSum(int[][]nums){
+  //use a nested loop to solve this
+  return null;//place holder return value so it compiles.
+}
+
+/**Rotate an array by returning a new array with the rows and columns swapped.
+  * You may assume the array is rectangular and neither rows nor cols is 0.
+  * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+  */
+public static int[][] swapRC(int[][]nums){
+  return new int[1][1];
+}
+public static void main(String[] args) {
+  System.out.println("" + arrToString2D())
+}
+}
--- MyArrays.java
+++ MyArrays.java
@@ -12,13 +12,13 @@ return copy; //return the new copy
 }
 // concatArray returns an array with the elements of two different arrays
 public static int [] concatArray(int [] ary1, int[] ary2){
-int[] result = new int [ar1.length + ar2.length]; //new arr made from ary1 & ary2
-for(int i =0;i<ar1.length;i++){
-result[i] = ar1[i];
+int[] result = new int [ary1.length + ary2.length]; //new arr made from ary1 & ary2
+for(int i =0;i<ary1.length;i++){
+result[i] = ary1[i];
 }//get all the elements of ary1 to put in new array
-for(i=0;i<ary2.length;i++){
+for(int i=0;i<ary2.length;i++){
 result[ary1.length + i] = ary2[i]; // get all elements from ary2
 }
 return result;
 }
-
+}
