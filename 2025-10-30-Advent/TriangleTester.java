f91d16dec39f1cf8a6778349ac02f64bde826d2f
Amy Mitchneck <amitchneck70@stuy.edu>
2025-11-02 21:46:45 -0500

    added countTrianglesB and A

--- 2025-10-30-Advent/TriangleTester.java
+++ 2025-10-30-Advent/TriangleTester.java
@@ -26,8 +26,39 @@ public class TriangleTester{
   public static boolean triangleCheck(int a, int b, int c){
     return ((a + b) > c && (a + c) > b && (b + c) > a);
   }
-
-  public static int countTrianglesA(String filename){
-    return 0;
-  }
+public static int countTrianglesA(String filename) {
+  int count = 0;
+  try(Scanner in = new Scanner(new File(filename))){
+    while (in.hasNextLine()) {
+      String[] nums = in.nextLine().trim().split("\\s+");
+      if (triangleCheck(Integer.parseInt(nums[0]), Integer.parseInt(nums[1]),
+              Integer.parseInt(nums[2])))
+        count++;
+    }
+} catch(FileNotFoundException e){
+    System.out.println("File not found");
+ }
+ return count;
 }
+// pt B
+public static int countTrianglesB(String filename) {
+  int count = 0;
+  try (Scanner in = new Scanner(new File(filename))){
+    while (in.hasNextLine()) {
+      int[][] lines = new int[3][3];
+      for (int i = 0; i < 3; i++) {
+        String[] nums = in.nextLine().trim().split("\\s+");
+        for (int j = 0; j < 3; j++) lines[i][j] = Integer.parseInt(nums[j]);
+      }
+      for (int col = 0; col < 3; col++) {
+       if(triangleCheck(lines[0][col],lines[1][col], lines[2][col]))
+      count ++;
+    }
+  }
+}catch(FileNotFoundException e){
+  System.out.println("File not found");
+}
+return count;
+}
+}  
+ 
