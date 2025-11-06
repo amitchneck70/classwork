427790794b03953678c5cc60418020daf6217984
Amy Mitchneck <amitchneck70@stuy.edu>
2025-11-05 21:48:05 -0500

    completed Bird.java override methods

---
+++ 2025-11-05-Pets/Bird.java
@@ -0,0 +1,17 @@
+public class Bird extends Animal{
+  private double height;
+  private String color;
+  public Bird(String noise, int age, String name, double height, String color){
+    super(noise, age, name);
+    this.height = height;
+    this.color = color;
+  }
+  public void speak(){
+    super.speak();
+    System.out.println("My feathers are colored " +color + ".");
+    System.out.println("I am " + height + " inches tall.");
+  }
+public String getName(){
+  return "The Mighty " + super.getName();
+ }
+}
