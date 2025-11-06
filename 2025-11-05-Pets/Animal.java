ed6dfee639c9d17d9f0677673c31895fff9b7a31
Amy Mitchneck <amitchneck70@stuy.edu>
2025-11-05 21:42:29 -0500

    Animal.java complete

---
+++ 2025-11-05-Pets/Animal.java
@@ -0,0 +1,23 @@
+public class Animal{
+private String noise;
+private int age;
+private String name;
+
+public Animal(String noise, int age, String name){
+this.noise = noise;
+this.age = age;
+this.name = name;
+}
+
+public void speak(){
+System.out.println("My name is " + getName() + ".");
+System.out.println("I am  " +age+ " years old.");
+System.out.println("I say " + getNoise()+ ".";
+}
+public String getName(){
+return name;
+}
+public String getNoise(){
+  return noise;
+ }
+}
