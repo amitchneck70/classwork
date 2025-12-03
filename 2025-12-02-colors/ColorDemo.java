2b31d32dffe99201a3cdfc25ac5abf627d592fe1
Amy Mitchneck <amitchneck70@stuy.edu>
2025-12-03 06:00:21 -0500

    finished my main, basically just prints a bunch of diff colored astrix

--- 2025-12-02-colors/ColorDemo.java
+++ 2025-12-02-colors/ColorDemo.java
@@ -19,7 +19,30 @@ public class ColorDemo{
   public static void color(int foreground,  int background){
    System.out.print("\u001b[" + foreground + ";" + (background +10)+"m"); 
   }
-  //sleep helper
-
-
-  }
+  //sleep helper milliseconds
+  public static void sleep(int ms){
+    try{
+       Thread.sleep(ms);
+     } catch (Exception e){}
+    }
+  public static  void main(String[] args){
+    Random rng = new Random();
+    int rows = 10;
+    int cols = 30;
+    System.out.print(CLEAR_SCREEN + HIDE_CURSOR);
+   for(int t=0; t<20;t++){ //refresh
+     for(int r =1;r<=rows;r++){
+       for(int c = 1;c<=cols;c++){
+       go(r,c);   
+       int foreground = 31 + rng.nextInt(6); //red -> cyan
+       int background = 40 + rng.nextInt(6);//backgrounds 40-45
+       color(foreground, background);
+       System.out.print("*");//test if forground and background acc work
+       System.out.print(RESET);
+      }
+     }
+     sleep(200);
+   }
+   System.out.print(SHOW_CURSOR+ RESET);
+   }
+} 
