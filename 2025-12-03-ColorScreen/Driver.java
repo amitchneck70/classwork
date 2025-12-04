ff46467976dcc04e775d634fa928a8e3227da3a0
Amy Mitchneck <amitchneck70@stuy.edu>
2025-12-03 23:24:03 -0500

    Update Driver.java- drawBorder complete

--- 2025-12-03-ColorScreen/Driver.java
+++ 2025-12-03-ColorScreen/Driver.java
@@ -10,4 +10,21 @@ public class Driver{
    Text.go(31,0); //move cursor to line 31 so prompt is below screen
      System.out.println(Text.RESET);
   }
+  //draw the border
+  public static void drawBorder(){
+    for(int col = 1; col <= 80; col++){
+      Text.go(1,col); //move cursor to top row
+      Text.color(Text.BRIGHT, Text.CYAN);
+      System.out.print("#"); //print border char, makes it 1 char thick
+    }
+    for(int row = 2; row < 30;row++){
+      Text.go(row,1); //start @ row 2 column one bc top and bottom take up first
+      Text.color(Text.BRIGHT, Text.YELLOW); // bright yellow
+      Text.go(row,80); //right column
+      Text.color(Text.BRIGHT, Text.GREEN);
+      System.out.print("#");
+    }
+  }
+        
 }
+
