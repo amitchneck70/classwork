dcd948a7c0c8da4a637d36a090558f9dbb63dde0
Amy Mitchneck <amitchneck70@stuy.edu>
2025-12-03 23:29:27 -0500

    border was only printing on top and right, made changes to for loops each component (top down left right) must be looped seperatley

--- 2025-12-03-ColorScreen/Driver.java
+++ 2025-12-03-ColorScreen/Driver.java
@@ -3,10 +3,10 @@ public class Driver{
     System.out.print(Text.CLEAR_SCREEN); //clear terminal screen
     System.out.print(Text.HIDE_CURSOR); //hide cursor while draw
     drawBorder(); //border creation + border color
-    int [] numbers = generateRandNum(); //create 4 random ints
-    displayNum(numbers); // show the generated random num on the screen with colors
-    drawSeparator(); //horizontal line below the numbers
-    drawMiddle(); // draw something in the middle
+   // int [] numbers = generateRandNum(); //create 4 random ints
+   // displayNum(numbers); // show the generated random num on the screen with colors
+  //  drawSeparator(); //horizontal line below the numbers
+//    drawMiddle(); // draw something in the middle
    Text.go(31,0); //move cursor to line 31 so prompt is below screen
      System.out.println(Text.RESET);
   }
@@ -17,10 +17,17 @@ public class Driver{
       Text.color(Text.BRIGHT, Text.CYAN);
       System.out.print("#"); //print border char, makes it 1 char thick
     }
+     for(int col = 1; col <= 80; col++){
+      Text.go(30,col); //move cursor to top row
+      Text.color(Text.BRIGHT, Text.MAGENTA);
+      System.out.print("#"); //print border char, makes it 1 char thick
+    }
     for(int row = 2; row < 30;row++){
       Text.go(row,1); //start @ row 2 column one bc top and bottom take up first
       Text.color(Text.BRIGHT, Text.YELLOW); // bright yellow
-      Text.go(row,80); //right column
+      System.out.print("#");
+     } 
+       Text.go(row,80); //right column
       Text.color(Text.BRIGHT, Text.GREEN);
       System.out.print("#");
     }
