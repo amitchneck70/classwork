a86b2cba2a5e0770d0b3a3c77b9cfbf3694ed731
Amy Mitchneck <amitchneck70@stuy.edu>
2025-12-08 10:15:42 -0500

    removed the hp question from game just default to codeWarrior hp

--- 2025-12-05-Adventurer/Game.java
+++ 2025-12-05-Adventurer/Game.java
@@ -3,10 +3,8 @@ public class Game{
   public static void main(String [] args){
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter name for CodeWarrior");
-    //Read one line of user input
     String CodeWarriorName = userInput.nextLine();
-    System.out.println("Enter hp for Code Warrior");
-    int CodeWarriorHp = userInput.nextInt();  //Do something with the input
   Adventurer enemy = new CodeWarrior(CodeWarriorName, CodeWarriorHp);
+  Adventurer player = new Barbarian(BarbName, Barbhp);
   }
 }
