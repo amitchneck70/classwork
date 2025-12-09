76b4006bde920ac3d58d706f3f04dc7e5a063655
Amy Mitchneck <amitchneck70@stuy.edu>
2025-12-08 22:18:34 -0500

    fixed location of input close, game runs correctly

--- 2025-12-05-Adventurer/Game.java
+++ 2025-12-05-Adventurer/Game.java
@@ -28,5 +28,45 @@ public class Game{
      } 
   Adventurer enemy = new CodeWarrior(enemyName, enemyHp);
   Adventurer player = new Barbarian(playerName, playerHp);
+  // battle
+  while(player.getHP()> 0 && enemy.getHP() > 0){ //keep going until someone dies
+   System.out.println("\nPlayer: " + player.getName() + " " + player.getHP() + "/"+
+   player.getmaxHP()+"HP, " + player.getSpecial() + "/" + player.getSpecialMax()+
+   " " + player.getSpecialName()); 
+   System.out.println("Enemy: " + enemy.getName() + " " + enemy.getHP() + "/"+ 
+   enemy.getmaxHP()+"HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax()+ 
+   " " + enemy.getSpecialName());  
+   System.out.println("\nType: (a)ttack / (sp)ecial / (su)pport / quit"); 
+  String action = userInput.nextLine().toLowerCase();
+  if(action.equals("a") || action.equals("attack")){
+    System.out.println(player.attack(enemy));  
+  } else if(action.equals("sp") || action.equals("special")){
+    System.out.println(player.specialAttack(enemy));
+  } else if(action.equals("su") || action.equals("support")){ 
+    System.out.println(player.support()); 
+  } else if (action.equals("quit")){
+   System.out.println("player has quit the game");
+   break;
+ } else {
+   System.out.println("invalid  input, try again");
+  continue;
+ }
+ if (enemy.getHP() > 0){
+  int enemyAction = random.nextInt(3);
+  if(enemyAction ==0){ 
+  System.out.println(enemy.attack(player));
+  } else if (enemyAction == 1){
+   System.out.println(enemy.specialAttack(player));
+ }else{
+  System.out.println (enemy.support());
   }
+ }
+  if(player.getHP() <= 0){
+   System.out.println("\n" + player.getName() + " has been defeated");
+  }else if (enemy.getHP() <= 0){
+   System.out.println("\n" + enemy.getName() +  "has been defeated");
+ }
+}
+userInput.close();
+}
 }
