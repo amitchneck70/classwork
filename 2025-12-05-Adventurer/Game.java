197fbea3377760b59b62a24dc5ce3b0563356091
Amy Mitchneck <amitchneck70@stuy.edu>
2025-12-08 21:44:45 -0500

    added loops for both player and enemy to throw exception when there neg or zero hp input

--- 2025-12-05-Adventurer/Game.java
+++ 2025-12-05-Adventurer/Game.java
@@ -1,10 +1,32 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Game{
   public static void main(String [] args){
     Scanner userInput = new Scanner(System.in);
+    Random random = new Random(); // random obj for the unpredictable moves   
+    System.out.println("Enter name for Barbarian");
+    String playerName = userInput.nextLine();
+    int playerHp = 0;
+    while(playerHp <= 0){
+     try{
+        System.out.println("enter hp for Barbarian");
+        playerHp = Integer.parseInt(userInput.nextLine());
+        } catch (NumberFormatException e){
+        System.out.println("invalid hp, must be greater than zero, enter again ");
+       }
+     }
     System.out.println("Enter name for CodeWarrior");
-    String CodeWarriorName = userInput.nextLine();
-  Adventurer enemy = new CodeWarrior(CodeWarriorName, CodeWarriorHp);
-  Adventurer player = new Barbarian(BarbName, Barbhp);
+    String enemyName = userInput.nextLine();
+    int enemyHp=0;
+    while(enemyHp <= 0){
+     try{
+        System.out.println("enter hp for CodeWarrior");
+        enemyHp = Integer.parseInt(userInput.nextLine());
+        } catch (NumberFormatException e){
+        System.out.println("invalid hp, must be greater than zero, enter again ");
+       }
+     } 
+  Adventurer enemy = new CodeWarrior(enemyName, enemyHp);
+  Adventurer player = new Barbarian(playerName, playerHp);
   }
 }
